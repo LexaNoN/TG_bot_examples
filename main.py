dc313e01cfe2a55e5ace48d0d284d198d6b20669
LexaNoN <alexey.2005.odessit@gmail.com>
2022-03-19 22:49:54 +0200

    some new features

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from uuid import uuid4
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import config
 import json
+from fpdf import FPDF
+import os
+
+json_base = {}
+
+
+def start(update, context):
+    json_base[update.message.chat.id] = {}
+    update.message.reply_text("Для начала опроса введите /quiz")
 
 
 def put(update, context):
@@ -39,23 +48,58 @@ def load(update, context):
         context.user_data.update(json.load(json_file))
     update.message.reply_text("loaded " + str(context.user_data))
 
+
 def text(update, context):
-    id = update.effective_user.id
+    id = update.message.chat.id
+    msg_text = update.effective_message.text
     value = context.user_data.get("quiz_state", False)
     if not value:
         update.message.reply_text("Вопрос ещё не был задан")
+        return
     elif value == 1:
-        print("1")
-        context.user_data.update("text", "")
-        context.user_data.update("quiz_state", 2)
+        context.user_data["quiz_state"] = 2
+        json_base[id]["quiz1"]["answ1"] = msg_text
+        update.message.reply_text("Введите второй ответ!")
     elif value == 2:
-        print("2")
-    else:
-        print("netu")
+        context.user_data["quiz_state"] = False
+        json_base[id]["quiz1"]["answ2"] = msg_text
+        update.message.reply_text("Спасибо за прохождение опроса!!")
+        createPDF(
+            [
+                json_base[id]["quiz1"]["answ1"],
+                json_base[id]["quiz1"]["answ2"]
+            ], id)
+        file_name = str(id) + ".pdf"
+        context.bot.send_document(chat_id=update.message.chat.id,
+                                  document=open(file_name, 'rb'),
+                                  filename="answers.pdf")
+        os.remove(file_name)
+
+
+def createPDF(list, id):
+    pdf = FPDF()
+    pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+    pdf.set_font('DejaVu', '', 14)
+    pdf.add_page()
+    pdf.cell(200, 10, txt="Пример ПДФ опросника", ln=1, align='C')
+    for i, j in enumerate(list):
+        pdf.cell(200, 10, txt="Ответ номер " + str(i + 1) + ": " + j, ln=i + 2, align='C')
+    pdf.output(str(id) + ".pdf", "F")
+
+
+def showAll(update, context):
+    update.message.reply_text(str(json_base))
+
 
 def quiz(update, context):
     context.user_data["quiz_state"] = 1
-    update.message.reply_text("Введите текст!")
+    json_base[update.message.chat.id]["quiz1"] = {}
+    update.message.reply_text("Введите первый ответ!")
+
+
+def test(update, context):
+    context.bot.send_document(chat_id=update.message.chat.id, document=open('666147669.pdf', 'rb'),
+                              filename="answers.pdf")
 
 
 if __name__ == '__main__':
@@ -67,6 +111,10 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('save', save))
     dp.add_handler(CommandHandler('load', load))
     dp.add_handler(CommandHandler('quiz', quiz))
-    dp.add_handler(MessageHandler(Filters.text, text))
+    dp.add_handler(CommandHandler('showAll', showAll))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('test', test))
+
+    dp.add_handler(MessageHandler((Filters.text & (~ Filters.command)), text))
     updater.start_polling()
     updater.idle()
